24b7ad6ba74dd5845bf74da9ad7932ba8dd3ee0e
kuzyuberdin <kuzyuberdin@gmail.com>
2024-11-13 01:49:01 +0400

    add first-step project

---
+++ first-step/src/app/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      hello
+        <button onClick={() => alert('HELLO')}>HELLO</button>
+    </div>
+  );
+}

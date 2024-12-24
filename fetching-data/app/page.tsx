d69b410ca633dad3aa32f2a20fddfa8635ac7a84
kuzyuberdin <kuzyuberdin@gmail.com>
2024-12-24 23:33:17 +0400

    add content for demo

--- fetching-data/app/page.tsx
+++ fetching-data/app/page.tsx
@@ -1,7 +1,20 @@
+'use client'
 export default function Home() {
+
+    const users = [
+        {name: 'John Doe', age: 30},
+        {name: 'Jane Doe', age: 28},
+        {name: 'Alice Doe', age: 35},
+    ]
+
+
   return (
       <div>
           <h1>HOME PAGE</h1>
+          {users.map((user, index) =><div key={index}>
+              <h2>{user.name}</h2>
+              <p>Age: {user.age}</p>
+          </div>)}
       </div>
   )
 }

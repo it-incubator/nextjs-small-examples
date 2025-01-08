00ef053ac2a9951a1448f5cf9b1d071ec00a0457
kuzyuberdin <kuzyuberdin@gmail.com>
2025-01-08 23:45:05 +0400

    add lesson1

---
+++ lesson1/src/store/store-wrapper.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import {ReactElement, ReactNode} from "react";
+import {store} from "@/store/store";
+import {Provider} from "react-redux";
+
+
+type ReactChild = ReactElement | undefined | null | ReactNode;
+
+export const StoreWrapper = ({children}: {children: ReactChild}) => {
+    return <Provider store={store}>
+        {children}
+    </Provider>
+}
